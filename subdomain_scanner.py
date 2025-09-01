e3f0ec567f645db28603515599101e9ad3b1f979
Matthew <mckip007@gmail.com>
2025-08-31 22:06:07 -0400

    Initial commit

--- subdomain_scanner.py
+++ subdomain_scanner.py
@@ -4,7 +4,37 @@ import requests
 # We need a list of subdomains to check for potential inlets
 SUBDOMAINS = ['www', 'mail', 'ftp', 'api', 'dev', 'test']
 
-# Adds subdomains+userinput+.com and if it finds something, it'll = IP address
+def extract_title(html):
+    start = html.lower().find("<title>")
+    if start == -1:
+        return None
+
+    ## start + 7 means it won't include "<title>" in the output
+    titlestart = start + 7
+
+    end = html.lower().find("</title>", titlestart)
+    if end == -1:
+        return None
+
+    title = html[titlestart:end]
+    cleaned = " ".join(title.split())
+    return cleaned
+
+def get_http_info(subdomain):
+    try:
+        # Here we are seeing if it is reachable through http
+        res = requests.get(f"http://{subdomain}", timeout=3)
+        title = extract_title(res.text)
+        if title:
+            print(f"    Title: {title}")
+        else:
+            print("    No title tag found")
+            except requests.exceptions.RequestException:
+            print("    No web response")
+    except requests.exceptions.RequestException:
+        print("    No web response")
+
+# Adds [SUBDOMAINS]+[userinput]+[.com] and if it finds something, it'll = IP address
 def check_subdomains(domain):
     for sub in SUBDOMAINS:
         url = f"{sub}.{domain}"
@@ -14,25 +44,6 @@ def check_subdomains(domain):
             get_http_info(url)
         except socket.gaierror:
             pass
-#
-def get_http_info(subdomain):
-    try:
-        # Here we are seeing if it is reachable through http
-        res = requests.get(f"http://{subdomain}", timeout=3)
-        if "<title>" in res.text:
-            print(f"    Title: {res.text.split('<title>')[1].split('</title>')[0]}")
-        else:
-            print("    No title tag found")
-    except Exception:
-     try:
-         # Here, if the above code fails, we check https for reachability
-        res = requests.get(f"https://{subdomain}", timeout=3)
-        if "<title>" in res.text:
-            print(f"    Title: {res.text.split('<title>')[1].split('</title>')[0]}")
-        else:
-            print("    No title tag found")
-     except Exception:
-        print("No web response")
 
 if __name__ == "__main__":
     domain = input("Enter domain (e.g. example.com): ").strip()
